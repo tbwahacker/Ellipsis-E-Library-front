e981728dd27189f51aa9357b7a82608191e25513
tbwahacker <twaloadam@gmail.com>
2023-07-18 19:36:20 +0300

    fix/documentation

--- app/src/main/java/com/anga/Elipsis_LMS/Constant.java
+++ app/src/main/java/com/anga/Elipsis_LMS/Constant.java
@@ -1,7 +1,10 @@
 package com.anga.Elipsis_LMS;
 
 public class Constant {
-    public static final String URL = "http://192.168.46.105:8000/";
+
+//    public static final String URL = "http://192.168.46.105:8000/";
+
+    public static final String URL = "https://ellipsiselibrary.000webhostapp.com/";
     public static final String HOME = URL+"api";
     public static final String LOGIN = HOME+"/login";
     public static final String LOGOUT = HOME+"/logout";
